426495e4de6d5ae96fcc6d9437a7bad64f72e28a
uygar <uygargurnal2@gmail.com>
2024-09-14 15:11:45 +0300

    initial b

--- main.py
+++ main.py
@@ -1,16 +1,18 @@
 import requests
 from send_mail import send_email
 
+topic = "tesla"
 api_key = "18a8b092b82c4baa81cabfa5124bac45"
-url = "https://newsapi.org/v2/everything?q=tesla&from=" \
-      "2024-08-14&sortBy=publishedAt&apiKey=18a8b092b82c4baa81cabfa5124bac45"
+url = f"https://newsapi.org/v2/everything?q={topic}&from=" \
+      "2024-08-14&sortBy=publishedAt&apiKey=18a8b092b82c4baa81cabfa5124bac45&language=en"
 
 request = requests.get(url)
 content = request.json()
 body = ""
-for article in content["articles"]:
+for article in content["articles"][:20]:
     if article["title"] is not None:
-        body = body + article["title"] + "\n" + article["description"] + 2*"\n"
+        body = "Subject: News" + "\n" + body + article["title"] + "\n" + article["description"] + "\n" + article["url"]\
+               + 2*"\n"
 
 gdr = body.encode("utf-8")
 send_email(gdr)
